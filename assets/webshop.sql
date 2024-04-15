b32f220bb606b3a886b378b434472c3ac3ca5286
Anton Boutkam <anton@nui-boutkam.nl>
2024-04-15 12:47:04 +0200

    tabellen

--- assets/webshop.sql
+++ assets/webshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Gegenereerd op: 09 apr 2024 om 10:20
+-- Gegenereerd op: 15 apr 2024 om 12:46
 -- Serverversie: 8.3.0
 -- PHP-versie: 8.3.4
 
@@ -28,17 +28,32 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `sale_order` (
-  `id` int NOT NULL,
-  `customer_id` int DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `street` varchar(255) NOT NULL,
-  `number` varchar(10) NOT NULL,
-  `number_add` varchar(6) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `postal` varchar(255) NOT NULL,
-  `country` varchar(255) NOT NULL,
-  `is_completed` tinyint(1) NOT NULL DEFAULT '0'
+                              `id` int NOT NULL,
+                              `customer_id` int DEFAULT NULL,
+                              `name` varchar(255) DEFAULT NULL,
+                              `email` varchar(255) DEFAULT NULL,
+                              `street` varchar(255) DEFAULT NULL,
+                              `number` varchar(10) DEFAULT NULL,
+                              `number_add` varchar(6) DEFAULT NULL,
+                              `city` varchar(255) DEFAULT NULL,
+                              `postal` varchar(255) DEFAULT NULL,
+                              `country` varchar(255) DEFAULT NULL,
+                              `is_completed` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `sale_order_item`
+--
+
+CREATE TABLE `sale_order_item` (
+                                   `id` int NOT NULL,
+                                   `sale_order_id` int NOT NULL,
+                                   `product_id` int DEFAULT NULL,
+                                   `article_number` varchar(255) DEFAULT NULL,
+                                   `description` varchar(255) DEFAULT NULL,
+                                   `price` decimal(12,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -49,7 +64,14 @@ CREATE TABLE `sale_order` (
 -- Indexen voor tabel `sale_order`
 --
 ALTER TABLE `sale_order`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `sale_order_item`
+--
+ALTER TABLE `sale_order_item`
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `sale_order_id` (`sale_order_id`);
 
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
@@ -59,7 +81,23 @@ ALTER TABLE `sale_order`
 -- AUTO_INCREMENT voor een tabel `sale_order`
 --
 ALTER TABLE `sale_order`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `sale_order_item`
+--
+ALTER TABLE `sale_order_item`
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `sale_order_item`
+--
+ALTER TABLE `sale_order_item`
+    ADD CONSTRAINT `sale_order_item_ibfk_1` FOREIGN KEY (`sale_order_id`) REFERENCES `sale_order` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
